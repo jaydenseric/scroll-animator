9778da2856e3637d4eb79808f92d26cb645a99a5
Jayden Seric <me@jaydenseric.com>
2021-05-24 20:33:19 +1000

    Test the private function `easeInOutCubic`.

--- test/index.mjs
+++ test/index.mjs
@@ -1,8 +1,10 @@
 import TestDirector from 'test-director';
 import testBundle from './bundle.test.mjs';
+import testEaseInOutCubic from './private/easeInOutCubic.test.mjs';
 
 const tests = new TestDirector();
 
+testEaseInOutCubic(tests);
 testBundle(tests);
 
 tests.run();
---
+++ test/private/easeInOutCubic.test.mjs
@@ -0,0 +1,24 @@
+import { strictEqual } from 'assert';
+import easeInOutCubic from '../../private/easeInOutCubic.js';
+
+export default (tests) => {
+  tests.add('`easeInOutCubic` at 0.', () => {
+    strictEqual(easeInOutCubic(0), 0);
+  });
+
+  tests.add('`easeInOutCubic` at 0.25.', () => {
+    strictEqual(easeInOutCubic(0.25), 0.0625);
+  });
+
+  tests.add('`easeInOutCubic` at 0.5.', () => {
+    strictEqual(easeInOutCubic(0.5), 0.5);
+  });
+
+  tests.add('`easeInOutCubic` at 0.75.', () => {
+    strictEqual(easeInOutCubic(0.75), 0.9375);
+  });
+
+  tests.add('`easeInOutCubic` at 1.', () => {
+    strictEqual(easeInOutCubic(1), 1);
+  });
+};
